e5d4d103ef03398ba3f90e87d4d94e7b26618390
ArshakAhamad <arshakahamad55@gmail.com>
2026-09-24 16:30:22 +0400

    chore: scaffold Next.js 15, Payload 3 and Tailwind 4 project

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If media moves to S3 / Vercel Blob, add its hostname here.
+  images: { remotePatterns: [] },
+}
+
+export default withPayload(nextConfig)
